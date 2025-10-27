1e8466a63f8e4de943c0a31200ce19aaf303a28b
Anthasss <anthasprog@gmail.com>
2025-10-27 11:41:43 +0800

    placed the api in services folder instead of the utils folder

--- src/hooks/useAuthWithBackend.js
+++ src/hooks/useAuthWithBackend.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import apiClient from '../utils/api';
+import apiClient from '../services/api';
 
 export const useAuthWithBackend = () => {
   const auth0 = useAuth0();
